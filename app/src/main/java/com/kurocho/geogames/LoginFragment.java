ffedf47bc7cc3767d8414334139fc1bcd5d9d5e8
Wojciech Góralczyk <goralczyk4@gmail.com>
2018-11-25 22:50:26 +0100

    LoginActivity changed to fragment

--- app/src/main/java/com/kurocho/geogames/LoginFragment.java
+++ app/src/main/java/com/kurocho/geogames/LoginFragment.java
@@ -1,17 +1,22 @@
 package com.kurocho.geogames;
 
 import android.arch.lifecycle.ViewModelProviders;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AutoCompleteTextView;
-import android.widget.Button;
 import android.widget.EditText;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import com.kurocho.geogames.viewmodels.LoginViewModel;
 
-public class LoginActivity extends AppCompatActivity {
+
+public class LoginFragment extends Fragment {
 
     @BindView(R.id.login_username)
     AutoCompleteTextView username;
@@ -19,19 +24,26 @@ public class LoginActivity extends AppCompatActivity {
     @BindView(R.id.login_password)
     EditText password;
 
-
     private LoginViewModel viewModel;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
-        ButterKnife.bind(this);
         viewModel = ViewModelProviders.of(this).get(LoginViewModel.class);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.login, container, false);
+        ButterKnife.bind(this, view);
+        return view;
     }
 
     @OnClick(R.id.login_sign_in_button)
     public void logInOnClick(){
 
     }
+
+
 }
