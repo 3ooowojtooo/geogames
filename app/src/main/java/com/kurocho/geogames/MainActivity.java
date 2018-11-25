e414066fe319ad4b16e0edd58fcc06c1ceff7bbf
Wojciech Góralczyk <goralczyk4@gmail.com>
2018-11-25 23:14:59 +0100

    MainActivity refractored

--- app/src/main/java/com/kurocho/geogames/MainActivity.java
+++ app/src/main/java/com/kurocho/geogames/MainActivity.java
@@ -2,26 +2,30 @@ package com.kurocho.geogames;
 
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import com.crashlytics.android.Crashlytics;
 import com.crashlytics.android.core.CrashlyticsCore;
 import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView mTextMessage;
 
-    BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+    @BindView(R.id.navigation)
+    BottomNavigationView navigation;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = (item) -> {
             switch (item.getItemId()) {
                 case R.id.navigation_search:
-                    mTextMessage.setText(R.string.title_search);
+
                     return true;
                 case R.id.navigation_my_games:
-                    mTextMessage.setText(R.string.title_my_games);
+
                     return true;
                 case R.id.navigation_sign_in:
-                    mTextMessage.setText(R.string.title_sign_in);
+
                     return true;
             }
             return false;
@@ -31,14 +35,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
         // Stop crashlytics for developing
         CrashlyticsCore core = new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build();
         Fabric.with(this, new Crashlytics.Builder().core(core).build());
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        if(savedInstanceState == null){
+            changeDisplayedFragmentToLoginFragment();
+        }
+    }
+
+    private void changeDisplayedFragmentToLoginFragment(){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        LoginFragment loginFragment = new LoginFragment();
+        transaction.replace(R.id.main_activity_fragment_container, loginFragment).commit();
     }
 
 }
