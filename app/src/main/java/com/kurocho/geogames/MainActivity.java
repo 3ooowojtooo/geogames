2d3951b0ba523196561dcf4bba950c070c36aded
kurocho <miugiel@gmail.com>
2018-11-25 16:47:52 +0100

    java 8 compatibility setup

--- app/src/main/java/com/kurocho/geogames/MainActivity.java
+++ app/src/main/java/com/kurocho/geogames/MainActivity.java
@@ -1,24 +1,18 @@
 package com.kurocho.geogames;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
 import android.widget.TextView;
 import com.crashlytics.android.Crashlytics;
 import com.crashlytics.android.core.CrashlyticsCore;
 import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
-
     private TextView mTextMessage;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+    BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = (item) -> {
             switch (item.getItemId()) {
                 case R.id.navigation_search:
                     mTextMessage.setText(R.string.title_search);
@@ -31,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
                     return true;
             }
             return false;
-        }
     };
 
     @Override
@@ -43,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
         Fabric.with(this, new Crashlytics.Builder().core(core).build());
 
         mTextMessage = (TextView) findViewById(R.id.message);
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
