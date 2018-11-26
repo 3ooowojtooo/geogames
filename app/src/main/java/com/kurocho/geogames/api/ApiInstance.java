278626d3c273b1e9a735e1d453c30509c48ef4f1
Wojciech Góralczyk <goralczyk4@gmail.com>
2018-11-26 09:49:05 +0100

    ApiInstance singleton class made

---
+++ app/src/main/java/com/kurocho/geogames/api/ApiInstance.java
@@ -0,0 +1,19 @@
+package com.kurocho.geogames.api;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiInstance {
+    private static Api instance;
+
+    public Api getInstance(){
+        if(instance == null){
+            Retrofit retrofit = new Retrofit.Builder().
+                    baseUrl(Api.BASE_URL).
+                    addConverterFactory(GsonConverterFactory.create()).
+                    build();
+            instance = retrofit.create(Api.class);
+        }
+        return instance;
+    }
+}
